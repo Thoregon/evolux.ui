e624956d904b5022f9d827e50255a175a8f9e63e
Bernhard Lukassen <bl@bernhard-lukassen.com>
2020-03-24 14:19:26 +0100

    fixes for component path resolving

--- index.mjs
+++ index.mjs
@@ -8,3 +8,6 @@ export { default as Router }            from './lib/router/router.mjs';
 export { default as UIElement }         from './lib/uielement.mjs';
 export { default as UIElements }        from './lib/uielements.mjs';
 export { default as UIElementBuilder }  from './lib/builder/elementbuilder.mjs';
+
+import components                       from './@components';
+universe.addComponents(components);
