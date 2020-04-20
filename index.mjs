24b85e554e70beca455e0e8daa086364bd260046
Bernhard Lukassen <bl@bernhard-lukassen.com>
2020-04-20 13:22:27 +0200

    everblack, tru4d

--- index.mjs
+++ index.mjs
@@ -4,10 +4,10 @@
  * @author: blukassen
  */
 
-export { default as Router }            from './lib/router/router.mjs';
-export { default as UIElement }         from './lib/uielement.mjs';
-export { default as UIElements }        from './lib/uielements.mjs';
-export { default as UIElementBuilder }  from './lib/builder/elementbuilder.mjs';
+export { default as Router }                from './lib/router/router.mjs';
+export { UIElement, UIObservingElement }    from './lib/uielement.mjs';
+export { default as UIElements }            from './lib/uielements.mjs';
+export { default as UIElementBuilder }      from './lib/builder/elementbuilder.mjs';
 
-import components                       from './@components';
+import components                           from './@components';
 universe.addComponents(components);
