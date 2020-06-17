95b3cbce52d08a6ae8756ff47d3e07eade7f1172
Bernhard Lukassen <bl@bernhard-lukassen.com>
2020-06-17 17:36:14 +0200

    UI Base Elements, KARTE protocolhandlers, scripts, docu, fixes

--- index.mjs
+++ index.mjs
@@ -9,5 +9,8 @@ export { UIElement, UIObservingElement }    from './lib/uielement.mjs';
 export { default as UIElements }            from './lib/uielements.mjs';
 export { default as UIElementBuilder }      from './lib/builder/elementbuilder.mjs';
 
+// addons
+export { default as QRCode }                from './ext/qrcode.mjs';
+
 import components                           from './@components';
 universe.addComponents(components);
