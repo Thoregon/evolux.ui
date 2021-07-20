106b709db36498a333eba5dc5189686c32e306a6
Bernhard Lukassen <bl@bernhard-lukassen.com>
2021-07-20 21:51:48 +0200

    cleanup, fixes, introduce 'baseDB'

--- index.mjs
+++ index.mjs
@@ -4,10 +4,11 @@
  * @author: blukassen
  */
 
-export { default as BrowserRouter }         from './lib/router/browserrouter.mjs';
 export { UIElement }                        from './lib/uielement.mjs';
-export { default as UIElements }            from './lib/uielements.mjs';
 export { default as UIElementBuilder }      from './lib/builder/elementbuilder.mjs';
+
+// export { default as BrowserRouter }         from './lib/router/browserrouter.mjs';
+// export { default as UIElements }            from './lib/uielements.mjs';
 
 // addons
 export { default as QRCode }                from './ext/qrcode.mjs';
